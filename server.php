f46974b29b22c1570b41771366d75be56fa9e1f1
Nalisha Hettiarachchi <nalishahettiarachchi@Nalishas-MacBook-Pro.local>
2021-03-17 13:05:15 +0300

    after corrections

--- server.php
+++ server.php
@@ -1,11 +1,13 @@
 <?php
 
 session_start();
+
 //initialisign variables
 
 $username = "" ;
 $email = "";
-$passowrd = "";
+$password_1 = "";
+$password_2 = "";
 
 $errors = array();
 
@@ -14,35 +16,44 @@ $errors = array();
 $db = mysqli_connect('localhost','root','','db_cw_test') or die("could not connect to database");
 
 //register users
-
+if(isset($_POST['username'])) {
 $username = mysqli_real_escape_string($db,$_POST['username']); 
 $email = mysqli_real_escape_string($db,$_POST['email']); 
-$password = mysqli_real_escape_string($db,$_POST['password']); 
+$password_1 = mysqli_real_escape_string($db,$_POST['password_1']); 
 $password_2 = mysqli_real_escape_string($db,$_POST['password_2']);
+}
 
 //form validation
 
-if(empty($username)){array_push($errors,"User name is requried")};
-if(empty($email)) {array_push($errors,"email is requried")};
-if(empty($password)) {array_push($errors,"password is requried")};
-if($password_2 != $ $password) {array_push($errors,"password does not match")};
+if(empty($username))array_push($errors,"User name is requried");
+if(empty($email))array_push($errors,"email is requried");
+if(empty($password_1))array_push($errors,"password is requried");
+if($password_1 != $password_2)array_push($errors,"password does not match");
 
 // check db for existing users with same username
 
-$user_check_query = "SELECT * FROM user WHERE username = ' $username' or email ='$email' LIMIT 1";
+$user_check_query = "SELECT * FROM user WHERE username = '$username' or email ='$email' LIMIT 1";
+
 
 $results = mysqli_query($db,$user_check_query);
-$user = mysqli_fetch_assoc($results);
+$user = mysqli_fetch_assoc($result);
 
 if ($user) {
-    if ($user['username'] == $username) { array_push($errors,"username already exits");}
-    if ($user['email'] == $email) { array_push($errors,"email already exits");}
+    if ($user['username'] === $username) { array_push($errors,"username already exits");}
+    if ($user['email'] === $email) { array_push($errors,"email already exits");}
 }
-
-if (count ($eroors) == 0){
+//Register the user if no errros
+if (count ($errors) == 0){
     $password =md5($password); //this will encrypt the password
     $query = "INSERT INTO user ( username , email, password) VALUES ('$username' ,'$email' ,'$passowrd') ";
 
-    
+    mysqli_query($db,$query);
+    $_SESSION['username'] = $username ;
+    $_SESSION['success'] = "You are now logged in";
+
+    header('location :login.php');
 
 }
+
+
+?>
